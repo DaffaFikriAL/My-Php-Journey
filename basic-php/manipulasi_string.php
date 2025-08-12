01df297c25184c57d78f43a15b17b2967c044cb0
DaffaFikriAL <daffa.alfiansyah04@gmail.com>
2025-08-12 14:23:04 +0700

    menambahkan file manipulasi_string.php

---
+++ basic-php/manipulasi_string.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manipulasi String</title>
+</head>
+
+<body>
+    <?php
+    // Penggabungan String (Concatenation)
+    $namaDepan = 'Daffa';
+    $namaBelakang = 'Fikri';
+
+    // Cara Pertama operator titik ( . )
+    $namaLengkap = $namaDepan . ' ' . $namaBelakang;
+
+    // Cara kedua pakai interpolasi string
+    $fullName = "$namaDepan $namaBelakang";
+
+    // Pencarian string
+    $kalimat = "Belajar PHP bahasa pemrograman PHP";
+    $posisi = strpos($kalimat, "PHP");
+    $posisiAkhir = strrpos($kalimat, "PHP");
+
+    // Fungsi lain string
+    $kalimatBaru = str_replace("PHP", "Javascript", $kalimat);
+
+    // Panjang string
+    $panjangKalimat = strlen($kalimat);
+
+    // Uppercase (huruf besar semua)
+    $upperKalimat = strtoupper($kalimat);
+
+    // Lowercase (huruf kecil semua)
+    $lowerKalimat = strtolower($kalimat);
+
+    // substr
+    $subKalimat = substr($kalimat, 0, 15);
+    ?>
+
+    <h3><?= $namaLengkap; ?></h3>
+    <h3><?= $fullName; ?></h3>
+    <h3><?= $posisi; ?></h3>
+    <h3><?= $posisiAkhir; ?></h3>
+    <h3><?= $kalimatBaru; ?></h3>
+    <h3><?= $panjangKalimat; ?></h3>
+    <h3><?= $upperKalimat; ?></h3>
+    <h3><?= $lowerKalimat; ?></h3>
+    <h3><?= $subKalimat; ?></h3>
+</body>
+
+</html>

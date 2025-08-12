84e2beeae07190044bf9527a9351f70db16f54b6
DaffaFikriAL <daffa.alfiansyah04@gmail.com>
2025-08-12 13:41:33 +0700

    menambahkan file variables.php

---
+++ basic-php/variables.php
@@ -0,0 +1,12 @@
+<?php
+// Wajib diawali simbol dollar $nama
+// Dimulai dengan huruf atau underscore _
+// Bersifat case sensitive
+
+$nama = 'Daffa Fikri Alfiansyah ';  //Deklarasi variabel nilai string
+$umur = 24; //Deklarasi variabel nilai integer
+$mahasiswa = false; //Deklarasi variabel nilai boolean
+
+echo $nama;
+echo $umur;
+echo $mahasiswa;

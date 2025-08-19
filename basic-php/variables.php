d08c2a5851229863de1a76a696a118780e359f3b
DaffaFikriAL <daffa.alfiansyah04@gmail.com>
2025-08-19 20:42:35 +0700

    update file manipulisi, tipe data dan variables: tambah css

--- basic-php/variables.php
+++ basic-php/variables.php
@@ -1,12 +1,33 @@
-<?php
-// Wajib diawali simbol dollar $nama
-// Dimulai dengan huruf atau underscore _
-// Bersifat case sensitive
+<!DOCTYPE html>
+<html lang="en">
 
-$nama = 'Daffa Fikri Alfiansyah ';  //Deklarasi variabel nilai string
-$umur = 24; //Deklarasi variabel nilai integer
-$mahasiswa = false; //Deklarasi variabel nilai boolean
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+    </style>
+</head>
 
-echo $nama;
-echo $umur;
-echo $mahasiswa;
+<body>
+    <?php
+    // Wajib diawali simbol dollar $nama
+    // Dimulai dengan huruf atau underscore _
+    // Bersifat case sensitive
+
+    $nama = 'Daffa Fikri Alfiansyah ';  //Deklarasi variabel nilai string
+    $umur = 24; //Deklarasi variabel nilai integer
+    $mahasiswa = false; //Deklarasi variabel nilai boolean
+
+    echo $nama;
+    echo $umur;
+    echo $mahasiswa;
+
+    ?>
+</body>
+
+</html>
